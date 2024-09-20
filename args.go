bcabf15f92deddc92d1c22994bf4dfe966af7a27
DESKTOP-9KA65BL\gogogo <511268932@qq.com>
2024-09-20 15:03:40 +0800

    args

--- args.go
+++ args.go
@@ -1,5 +1,93 @@
 package cobra
 
+import (
+	"fmt"
+	"strings"
+)
 
-type PositionalArgs func (cmd *Command, args []string) error
+type PositionalArgs func(cmd *Command, args []string) error
 
+func legactArgs(cmd *Command, args []string) error {
+	if !cmd.HasSubCommands() {
+		return nil
+	}
+
+	if !cmd.HasParent() && len(args) > 0 {
+		return fmt.Errorf("unkonwn command %q for %q%s", args[0], cmd.CommandPath(), cmd.findSuggestion(args[0]))
+	}
+
+	return nil
+}
+
+func NoArgs(cmd *Command, args []string) error {
+	if len(args) > 0 {
+		return fmt.Errorf("unkonwn command %q for %q", args[0], cmd.CommandPath())
+	}
+	return nil
+}
+
+func OnlyValidArgs(cmd *Command, args []string) error {
+	if len(cmd.ValidArgs) > 0 {
+		validArgs := make([]string, 0, len(cmd.ValidArgs))
+		for _, v := range cmd.ValidArgs {
+			validArgs = append(validArgs, strings.SplitN(v, "\t", 2)[0])
+		}
+		for _, v := range args {
+			if !stringInSlice(v, validArgs) {
+				return fmt.Errorf("invalid argument %q for %q", v, cmd.CommandPath(), cmd.findSuggestion(args[0]))
+			}
+		}
+	}
+	return nil
+}
+
+func MinimumNArgs(n int) PositionalArgs {
+	return func(cmd *Command, args []string) error {
+		if len(args) > n {
+			return fmt.Errorf("require at least %d args(s), only received %q", n, len(args))
+		}
+		return nil
+	}
+}
+
+func MaximumNArgs(n int) PositionalArgs {
+	return func(cmd *Command, args []string) error {
+		if len(args) > 0 {
+			return fmt.Errorf("accepts at must %d args(s), received %d", n, len(args))
+		}
+		return nil
+	}
+}
+
+func ExactArgs(n int) PositionalArgs {
+	return func(cmd *Command, args []string) error {
+		if len(args) != n {
+			return fmt.Errorf("accepts %d arg(s), received %d", n, len(args))
+		}
+		return nil
+	}
+}
+
+func RangeArgs(min int, max int) PositionalArgs {
+	return func(cmd *Command, args []string) error {
+		if len(args) < min || len(args) > max {
+			return fmt.Errorf("accepts between %d and %d arg(s), received %d", min, max, len(args))
+		}
+		return nil
+	}
+}
+
+func MatchAll(pargs ...PositionalArgs) PositionalArgs {
+	return func(cmd *Command, args []string) error {
+		for _, parg := range pargs {
+			if err := parg(cmd, args); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func ExactValidArgs(n int) PositionalArgs {
+	return MatchAll(ExactArgs(n), OnlyValidArgs)
+}
